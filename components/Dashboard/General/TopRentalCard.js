e5ea44fce8a5a55556d2bce3625d91345b1e47ae
Cintia Ruiz <cintiaruizv@gmail.com>
2022-12-12 20:36:35 -0600

    adding top rental logic

--- components/Dashboard/General/TopRentalCard.js
+++ components/Dashboard/General/TopRentalCard.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
+const myLoader = ({ src }) => {
+  return `${src}`
+}
 
-export default function TopRentalCard ({ img, name, date, reserves }) {
+export default function TopRentalCard ({ img, name, date, reserves, price, model }) {
   return (
     <article className='col-12 mx-auto'>
       <section className='card mb-3 mt-4 general__cardTopRental shadow-sm d-flex'>
@@ -10,22 +13,26 @@ export default function TopRentalCard ({ img, name, date, reserves }) {
             <div className='col-4 col-md-2 mx-auto'>
               <Image
                 className='mt-3 mb-3'
+                loader={myLoader}
                 src={img}
                 alt='Scooter top rental'
                 width={71}
                 height={60}
               />
             </div>
-            <p className='col-4 col-md-3 mb-0'>
+            <p className='col-4 col-md-2 mb-0 fw-bold general__topRentalInfo '>
               {name}
             </p>
-            <p className='d-none d-md-block col-3 mb-0'>
-              Cancún, Quintana Roo
+            <p className='d-none d-md-block col-2 mb-0 general__topRentalInfo'>
+              $ {price} .00 p/día
             </p>
-            <p className='d-none d-md-block col-2 mb-0'>
+            <p className='d-none d-md-block col-2 mb-0 general__topRentalInfo'>
+              {model}
+            </p>
+            <p className='d-none d-md-block col-2 mb-0 general__topRentalInfo'>
               {date}
             </p>
-            <p className='col-4 col-md-2 mb-0'>{reserves} reservas</p>
+            <p className='col-4 col-md-2 mb-0 general__topRentalInfo'>{reserves} reservas</p>
           </div>
         </div>
       </section>
--- components/Dashboard/General/index.js
+++ components/Dashboard/General/index.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CardAmount from './CardAmount'
 import CustomDay from './CustomDay'
 import Search from './Search'
 import TopRentalCard from './TopRentalCard'
+import { format } from 'date-fns'
+import { getTopRentalReserves } from '/services/reserves/reserves'
+import { getAllReserves } from '/services/reserves/reserves'
+import { Link } from '@mui/material'
 
 export default function General () {
+  const [reserves, setReserves] = useState([])
+  const getTopRentals = async () => {
+    const token = localStorage.getItem('token')
+    try {
+      const response = await getTopRentalReserves(token)
+      const { data: { reserves } } = await response.json()
+      setReserves(reserves)
+    } catch (error) {}
+  }
+
+ const today = format(new Date(), 'dd/MMM/yyyy')
+
+  useEffect(() => {
+    getTopRentals()
+  }, [])
+
+  const getReserves = async () => {
+    const token = localStorage.getItem('token')
+    const user = localStorage.getItem('userCurrent')
+    try {
+      const response = await getAllReserves(token)
+      const {
+        data: { reserves }
+      } = await response.json()
+      setData(row(reserves))
+      setReserves(reserves)
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    getReserves()
+  }, [])
+
+  const getActualReserves = async () => {
+    const token = localStorage.getItem('token')
+    try {
+      
+      const response = await getAllReserves(token)
+      const {
+        data: { reserves }
+      } = await response.json()
+      setData(row(reserves))
+      setReserves(reserves)
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    getActualReserves()
+  }, [])
+
   return (
     <>
       <section className='container-fluid text-center text-md-start'>
@@ -15,30 +69,28 @@ export default function General () {
 
             <div className='container'>
               <div className='row'>
-                <CardAmount amount='15' title='Rentas actuales' />
-                <CardAmount amount='6' title='Rentas próximas' />
-                <CardAmount amount='3' title='Rentas canceladas' />
+                
+                <CardAmount amount={reserves.length} title='Total de reservas'/>
+                <CardAmount amount='6' title='Reservas actuales' />
+                <CardAmount amount='3' title='Reservas canceladas' />
 
               </div>
-              <h3 className='mt-5'>Top rentals</h3>
-              <TopRentalCard
-                img='/assets/images/flotebikers/vitalia-125.webp'
-                name='Scooter Vitalia 150 Azul'
-                date='Oct 23, 2022'
-                reserves='25'
-              />
-              <TopRentalCard
-                img='/assets/images/flotebikers/blue-scooter.webp'
-                name='Scooter Vitalia 150 Azul'
-                date='Oct 23, 2022'
-                reserves='20'
-              />
-              <TopRentalCard
-                img='/assets/images/flotebikers/scooter-blackblue.webp'
-                name='Scooter Vitalia 150 Azul'
-                date='Oct 23, 2022'
-                reserves='15'
-              />
+              <h3 className='mt-5'><i class='fa fa-star-o edit-icon fa-xs me-2' aria-hidden='true' />Top 5 rentals 
+ </h3>
+              {reserves
+              .map((reserve) => (
+                <TopRentalCard
+                key={reserve._id}
+                name={reserve.vehicle.name}
+                price={reserve.vehicle.price}
+                model={reserve.vehicle.model}
+                img={reserve.vehicle.image}
+                date= {`${today}`}
+                reserves={reserve.count}
+                />
+              ))
+            }
+
 
             </div>
           </section>
